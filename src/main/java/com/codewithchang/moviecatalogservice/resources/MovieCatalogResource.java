13611c230a4ae7013398eaf391e833890cdbb8df
Chang <cnxiong03@gmail.com>
2021-02-25 16:44:55 -0500

    able to use RestTemplate to retrieve data

---
+++ src/main/java/com/codewithchang/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,44 @@
+package com.codewithchang.moviecatalogservice.resources;
+
+import com.codewithchang.moviecatalogservice.models.CatalogItem;
+import com.codewithchang.moviecatalogservice.models.Movie;
+import com.codewithchang.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
+
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        })
+                .collect(Collectors.toList());
+
+        // For each movie ID, call movie info service and get details
+
+        // Put them all together
+
+    }
+}
