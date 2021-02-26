1bf16d475482826304066fafedce1d021497c66e
Chang <cnxiong03@gmail.com>
2021-02-26 17:34:37 -0500

    using asynchronous programming and constructs to do synchronous programming via webclient

--- src/main/java/com/codewithchang/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/codewithchang/moviecatalogservice/resources/MovieCatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,6 +23,9 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
@@ -32,6 +36,16 @@ public class MovieCatalogResource {
 
         return ratings.stream().map(rating -> {
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+            /* Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+
+             */
+
             return new CatalogItem(movie.getName(), "Test", rating.getRating());
         })
                 .collect(Collectors.toList());
