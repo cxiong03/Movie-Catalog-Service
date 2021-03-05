7fc51400bdf9517f7631f2c1bda1a8844185e538
Chang <cnxiong03@gmail.com>
2021-03-04 21:27:14 -0500

    with eureka connected to all our apps we changed our url instead of hard coding localhost

--- src/main/java/com/codewithchang/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/codewithchang/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,6 +2,7 @@ package com.codewithchang.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ public class MovieCatalogServiceApplication {
 	//The final thing that consolidate everything (our application - front end)
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
--- src/main/java/com/codewithchang/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/codewithchang/moviecatalogservice/resources/MovieCatalogResource.java
@@ -31,11 +31,11 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/foo" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/foo" + userId, UserRating.class);
 
         return ratings.getUserRating().stream().map(rating -> {
             // For each movie ID, call movie info service and get details
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
             // Put them all together
             return new CatalogItem(movie.getName(), "Test", rating.getRating());
         })
